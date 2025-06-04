9538a5f6f6bd51ca3b961b5390682627cb6623c4
Abhinavsketch <abhinavchaubey916@gmail.com>
2025-06-05 02:32:03 +0530

    Adding Downloading Logic

--- BACKEND/controllers/resumeController.js
+++ BACKEND/controllers/resumeController.js
@@ -2,11 +2,15 @@ import express from "express";
 import { generatePdf } from "../utils/makepdf.js";
 
 export const getUserData = async (req, res) => {
-  const userData = req.body;
-  const pdf = generatePdf(userData);
-  res.send({
-    "Content-Type": "application/pdf",
-    "Content-Disposition": "attachment; filename = resume.pdf",
-  });
-  res.send(pdf);
+  try {
+    const userData = req.body;
+    const pdf = await generatePdf(userData);
+    res.set({
+      "Content-Type": "application/pdf",
+      "Content-Disposition": "attachment; filename=resume.pdf",
+    });
+    res.send(pdf);
+  } catch (error) {
+    console.log(error);
+  }
 };
--- BACKEND/utils/makepdf.js
+++ BACKEND/utils/makepdf.js
@@ -1,8 +1,16 @@
 import express from "express";
 import puppeteer from "puppeteer";
+import ejs from "ejs";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const generatePdf = async (userData) => {
-  const html = await ejs.renderFile("user.ejs", { userData });
+  const html = await ejs.renderFile(join(__dirname, "../views/user.ejs"), {
+    userData,
+  });
   const browser = await puppeteer.launch({
     headless: "new",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
